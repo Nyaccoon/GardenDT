789465751854342316ca8793533cc1ba0ce3a711
zlaticaa <cstudyspace@gmail.com>
2026-06-11 14:50:04 +0200

    Revert "Merge branch '3d-grid' into testing"
    This reverts commit 0f50d04e1582cb6bde66eb184e5ce29c46bcd8d1, reversing
    changes made to 12c490a453b50928055493a25b4cc5aabf411b99.

--- Assets/Scripts/BuildingSystem/BuildingData.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Data/Building")]
-public class BuildingData : ScriptableObject
-{
-    [field: SerializeField] public BuildingModel model { get; private set; }
-    [field: SerializeField] public Support.FloorType floorType { get; private set; }
-}
